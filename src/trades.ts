92b1148c162270507d89574d3f0e0441aa38dfd6
Paul Cotogno <pcotogno13@gmail.com>
2026-06-20 13:09:53 +0200

    fix timeout trade

--- src/trades.ts
+++ src/trades.ts
@@ -60,7 +60,7 @@ export const tradeCard = async ({ email, mdp }: Account) => {
         }
 
         if (i === 0) {
-          await page.locator('body > div:last-of-type > div > div:nth-child(4) > div > div > div:nth-child(2) > div > button').setTimeout(300).click()
+          await page.locator('body > div:last-of-type > div > div:nth-child(4) > div > div > div:nth-child(2) > div > button').setTimeout(1000).click()
             .catch(e => {
               console.log('CATCH CLICK ON FIRST CARD');
               resolve(() => {});
